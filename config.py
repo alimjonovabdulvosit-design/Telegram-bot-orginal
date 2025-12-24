ae286f3dc38b5e0895f21ee6b78cda820e211e00
Alimjonov Abdulvosit <alimjonovabdulvosit@gmail.com>
2025-12-24 11:51:47 +0500

    Update: New bot token and channel ID defaults

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
-CHANNEL_ID = int(os.getenv("CHANNEL_ID", 0))
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8529469877:AAHb5ct0exDCeZzOsRUtshXScjvyH4LI3EE")
+ADMIN_ID = int(os.getenv("ADMIN_ID", 812242774))
+CHANNEL_ID = int(os.getenv("CHANNEL_ID", -1003533914629))
 DATA_DIR = os.getenv("DATA_DIR", "/app/data")
 
 # Export to environment for other modules
